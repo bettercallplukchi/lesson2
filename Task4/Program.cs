19e1a1b69d3550e313fe7b18916a4e7216102708
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-25 20:18:25 +0200

    Добавил 4 задание

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = int.Parse(Console.ReadLine()!);
+
+if (a >= 100)
+{
+    int num = a / 100 % 10;
+    Console.WriteLine(num);
+}
+else
+{
+    if (a < 100 || a > 1000)
+    {
+        Console.WriteLine("NON-ODZ");
+    }
+}
