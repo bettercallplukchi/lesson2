5a5a59576b4c24e28db89dc4d9eab39d7ddae6f7
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-26 01:35:19 +0200

    Добавил второе задание

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("true");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num < 1000)
+{
+    int num1 = num % 100 / 10;
+    int num2 = num % 10;
+    double result = Math.Pow(num1, num2);
+    Console.WriteLine(result);
+}
+else
+{
+    if (num < 100 || num > 1000)
+    {
+        Console.WriteLine("false");
+    }
+}
