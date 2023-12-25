626856921d05ad60d9cf03e90f5f9a9889d7b1a5
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-25 20:05:52 +0200

    Добавили 3 задание

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine()!);
+int m = int.Parse(Console.ReadLine()!);
+
+if (n % m == 0)
+{
+    Console.WriteLine("CRATNO");
+}
+else
+{
+    Console.WriteLine("NECRATNO " + n % m);
+}
+
