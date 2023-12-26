f26bb16741f39bc1b0e26ead0caafd234e0ec736
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-26 16:47:45 +0200

    Добавил домашнее задание с первой задачей

---
+++ HomeTaskLesson2/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = int.Parse(Console.ReadLine()!);
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    Console.WriteLine("number can be bit by 7 and 23 both");
+}
+else
+{
+    Console.WriteLine("It can not be used");
+}
