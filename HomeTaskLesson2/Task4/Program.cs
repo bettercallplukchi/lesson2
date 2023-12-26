77bd7fa98891cf004617c9fc81ccb13e2fabaed7
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-26 17:32:39 +0200

    Добавил домашнее задание с четвёртой задачей

---
+++ HomeTaskLesson2/Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter natural number - n");
+int number = int.Parse(Console.ReadLine()!);
+
+string numberStr = number.ToString();
+string result = "";
+for (int i = 0; i < numberStr.Length; i++)
+{
+    Console.Write(numberStr[i]);
+
+    if (i < numberStr.Length - 1)
+    {
+        Console.Write(", ");
+    }
+}
