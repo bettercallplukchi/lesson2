15d395c6c7e84c85bfc3141c1e835ceeb79627ed
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-26 17:19:41 +0200

    Добавил домашнее задание с третьей задачей

---
+++ HomeTaskLesson2/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter number from 10 to 99");
+int num1 = int.Parse(Console.ReadLine()!);
+int maxnum = 0;
+
+if (num1 > 9 && num1 < 100)
+{
+    int last = num1 % 10;
+    maxnum = Math.Max(last, maxnum);
+    num1 /= 10;
+    Console.WriteLine(maxnum);
+
+}
+else
+{
+    Console.WriteLine("incorrect number");
+}
