9b0c2d21f214a973e3d9a845f46309ee30224318
Sasha Plukchi <loxpojizniia@gmail.com>
2023-12-26 17:07:25 +0200

    Добавил домашнее задание со второй задачей

---
+++ HomeTaskLesson2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход координаты точки (Х и У),
+// причём Х неравно 0 и У неравно О и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+Console.WriteLine("Enter x and y");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine());
+
+if (num1 > 0 && num2 > 0)
+{
+    Console.WriteLine("You are in the first part");
+}
+else if (num1 < 0 && num2 > 0)
+{
+    Console.WriteLine("You are in the second part");
+}
+else if (num1 < 0 && num2 < 0)
+{
+    Console.WriteLine("You are in the third part");
+}
+else if (num1 > 0 && num2 < 0)
+{
+    Console.WriteLine("You are in the fourth part");
+}
+
