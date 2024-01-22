568d81691027c3bb4d50d7e3ab0d70d9454b24fd
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-22 15:41:45 +0200

    Добавил второе дз

---
+++ DZ_3UROK/task2/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 2: Задайте массив заполненный случайными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+using System.CodeDom.Compiler;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("enter quantity: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Print(" ");
+        int[] arr = GenerateRandomArray(n, 100, 500);
+        foreach (int num in arr)
+        {
+            Print("Элемент : " + num.ToString());
+        }
+        int count = EvenElements(arr);
+        {
+            Print("Количество четных элементов : " + count.ToString());
+        }
+    }
+
+    public static void Print(string res)
+    {
+        Console.WriteLine(res);
+    }
+
+    static int[] GenerateRandomArray(int n, int min_val, int max_val)
+    {
+        int[] arr = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = random.Next(min_val, max_val + 1);
+        }
+        return arr;
+    }
+
+    static int EvenElements(int[] arr)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+
+        }
+        return count;
+        {
+
+        }
+    }
+}
+
