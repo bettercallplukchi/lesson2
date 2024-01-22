1522a3e8f94090be032d688eec60057fe3dabe6b
Sasha Plukchi <loxpojizniia@gmail.com>
2024-01-22 15:43:01 +0200

    Добавил третье дз

---
+++ DZ_3UROK/task3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter quantity: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = GenerateRandomArray(n, 1, 9);
+        Console.WriteLine("source array : ");
+        Print(arr);
+
+
+        for (int i = 0; i < n / 2; i++)
+        {
+            int min_ind = arr[i];
+            arr[i] = arr[n - i - 1];
+            arr[n - i - 1] = min_ind;
+        }
+        Console.WriteLine("reverse array :");
+        Print(arr);
+
+
+
+    }
+    static void Print(int[] arr)
+    {
+        foreach (int number in arr)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+    static int[] GenerateRandomArray(int n, int min_v, int max_v)
+    {
+        int[] arr = new int[n];
+        Random rand = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = rand.Next(min_v, max_v + 1);
+        }
+        return arr;
+    }
+
+
+
+}
+
+
